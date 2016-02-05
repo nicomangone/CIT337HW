30c455383036e434b2a675f87969f5526fd98682
Nico Mangone <namangone@oakland.edu>
2016-02-04 22:16:54 -0500

    implementing main mathod only

--- BeerSong.java
+++ BeerSong.java
@@ -10,34 +10,8 @@
 public class BeerSong {
     
     public static void main(String[] args) {
-        Ninety_Nine_Bottles_of_Beer();
+      //  Ninety_Nine_Bottles_of_Beer();
 
 }
     
-    public static void Ninety_Nine_Bottles_of_Beer() {
-        
-        int beerNum = 99;
-        String word = "bottles";
-
-     while (beerNum > 0) {
-
-        if (beerNum == 1) {
-          word = "bottle"; 
-        }
-
-        System.out.print(beerNum + " " + word + " of beer on the wall");
-        System.out.println(", " + beerNum + " " + word + " of beer.");
-        System.out.print("Take one down and ");
-        System.out.print("pass it around, ");
-        beerNum = beerNum - 1;
-
-        //create if statement for when song ends
-        if (beerNum > 0) {
-           System.out.println(beerNum + " " + word + " of beer on the wall.");
-           System.out.println(" ");
-        } else {
-           System.out.println("no more bottles of beer on the wall.");
-        } 
-     } 
-   } 
 }
